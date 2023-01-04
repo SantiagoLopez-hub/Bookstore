c2de3e6bad10825750a887263aff4b858896b303
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2023-01-04 18:47:49 +0000

    Start application with test data

--- Bookstore/src/main/java/com/santiago/bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/santiago/bookstore/BookstoreApplication.java
@@ -1,7 +1,13 @@
 package com.santiago.bookstore;
 
+import com.santiago.bookstore.model.Author;
+import com.santiago.bookstore.model.Book;
+import com.santiago.bookstore.repo.AuthorRepo;
+import com.santiago.bookstore.repo.BookRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -9,4 +15,15 @@ public class BookstoreApplication {
         SpringApplication.run(BookstoreApplication.class, args);
     }
 
+     @Bean
+     public CommandLineRunner runner(BookRepo bookRepo, AuthorRepo authorRepo) {
+         return args -> {
+             authorRepo.save(Author.builder().id(1L).name("Rodrigo").build());
+
+             bookRepo.save(Book.builder().id(1L).title("The Lord of the Rings")
+                             .author(authorRepo.findById(1L).get().getName()).build());
+             bookRepo.save(Book.builder().id(2L).title("The Hobbit").build());
+             bookRepo.save(Book.builder().id(3L).title("The Silmarillion").build());
+         };
+     }
 }
