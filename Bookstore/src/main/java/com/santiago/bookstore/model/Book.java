3ca879f7f7c5d272ba3adf004857ce65645e5f57
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2023-01-04 17:02:28 +0000

    Create Book.java

---
+++ Bookstore/src/main/java/com/santiago/bookstore/model/Book.java
@@ -0,0 +1,40 @@
+package com.santiago.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Book {
+    @Id
+    private Long id;
+
+    private Double price;
+
+    private String title;
+    private String isbn;
+    private String author;
+    private String publisher;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Book book = (Book) o;
+        return id != null && Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
